c9778dc715b843847bf9be5203dad4cb6dbccc59
anh0001 <anhrisn@gmail.com>
2016-02-27 11:32:08 +0700

    Added initial cmake files.

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,70 @@
+# This list is required for static linking and exported to DeepSTDConfig.cmake
+set(DEEPSTD_LINKER_LIBS "")
+
+# ---[ Boost
+find_package(Boost 1.46 REQUIRED COMPONENTS system thread filesystem)
+include_directories(SYSTEM ${Boost_INCLUDE_DIR})
+list(APPEND DeepSTD_LINKER_LIBS ${Boost_LIBRARIES})
+
+# ---[ OpenCV
+if(USE_OPENCV)
+  find_package(OpenCV QUIET COMPONENTS core highgui imgproc imgcodecs)
+  if(NOT OpenCV_FOUND) # if not OpenCV 3.x, then imgcodecs are not found
+    find_package(OpenCV REQUIRED COMPONENTS core highgui imgproc)
+  endif()
+  include_directories(SYSTEM ${OpenCV_INCLUDE_DIRS})
+  list(APPEND DEEPSTD_LINKER_LIBS ${OpenCV_LIBS})
+  message(STATUS "OpenCV found (${OpenCV_CONFIG_PATH})")
+  add_definitions(-DUSE_OPENCV)
+endif()
+
+# ---[ Python
+if(BUILD_python)
+  if(NOT "${python_version}" VERSION_LESS "3.0.0")
+    # use python3
+    find_package(PythonInterp 3.0)
+    find_package(PythonLibs 3.0)
+    find_package(NumPy 1.7.1)
+    # Find the matching boost python implementation
+    set(version ${PYTHONLIBS_VERSION_STRING})
+    
+    STRING( REGEX REPLACE "[^0-9]" "" boost_py_version ${version} )
+    find_package(Boost 1.46 COMPONENTS "python-py${boost_py_version}")
+    set(Boost_PYTHON_FOUND ${Boost_PYTHON-PY${boost_py_version}_FOUND})
+    
+    while(NOT "${version}" STREQUAL "" AND NOT Boost_PYTHON_FOUND)
+      STRING( REGEX REPLACE "([0-9.]+).[0-9]+" "\\1" version ${version} )
+      
+      STRING( REGEX REPLACE "[^0-9]" "" boost_py_version ${version} )
+      find_package(Boost 1.46 COMPONENTS "python-py${boost_py_version}")
+      set(Boost_PYTHON_FOUND ${Boost_PYTHON-PY${boost_py_version}_FOUND})
+      
+      STRING( REGEX MATCHALL "([0-9.]+).[0-9]+" has_more_version ${version} )
+      if("${has_more_version}" STREQUAL "")
+        break()
+      endif()
+    endwhile()
+    if(NOT Boost_PYTHON_FOUND)
+      find_package(Boost 1.46 COMPONENTS python)
+    endif()
+  else()
+    # disable Python 3 search
+    find_package(PythonInterp 2.7)
+    find_package(PythonLibs 2.7)
+    find_package(NumPy 1.7.1)
+    find_package(Boost 1.46 COMPONENTS python)
+  endif()
+  if(PYTHONLIBS_FOUND AND NUMPY_FOUND AND Boost_PYTHON_FOUND)
+    set(HAVE_PYTHON TRUE)
+    if(BUILD_python_layer)
+      add_definitions(-DWITH_PYTHON_LAYER)
+      include_directories(SYSTEM ${PYTHON_INCLUDE_DIRS} ${NUMPY_INCLUDE_DIR} ${Boost_INCLUDE_DIRS})
+      list(APPEND DEEPSTD_LINKER_LIBS ${PYTHON_LIBRARIES} ${Boost_LIBRARIES})
+    endif()
+  endif()
+endif()
+
+# ---[ Doxygen
+if(BUILD_docs)
+  find_package(Doxygen)
+endif()
---
+++ features/mser/_mser.cpp
@@ -0,0 +1,28 @@
+#include <Python.h>  // NOLINT(build/include_alpha)
+
+// Produce deprecation warnings (needs to come before arrayobject.h inclusion).
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+#include <boost/make_shared.hpp>
+#include <boost/python.hpp>
+#include <boost/python/raw_function.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <numpy/arrayobject.h>
+
+// these need to be included after boost on OS X
+#include <string>  // NOLINT(build/include_order)
+#include <vector>  // NOLINT(build/include_order)
+#include <fstream>  // NOLINT
+
+#include "./caffe.hpp"
+
+// Temporary solution for numpy < 1.7 versions: old macro, no promises.
+// You're strongly advised to upgrade to >= 1.7.
+#ifndef NPY_ARRAY_C_CONTIGUOUS
+#define NPY_ARRAY_C_CONTIGUOUS NPY_C_CONTIGUOUS
+#define PyArray_SetBaseObject(arr, x) (PyArray_BASE(arr) = (x))
+#endif
+
+namespace bp = boost::python;
+
+
